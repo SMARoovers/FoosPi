cd7e63f2ba6bca8f8bfa60891947941ab0978060
Bas Roovers <mail@basroovers.nl>
2014-01-11 18:42:09 +0100

    Optimize

--- foospi.py
+++ foospi.py
@@ -26,6 +26,8 @@ print "Chosen venue is " + venue
 logger.info("Chosen venue is " + venue)
 
 webserver = 'foosball.vicompany.local'
+goal = 0
+buttonDown = 0
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -36,69 +38,80 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 Handler = MyRequestHandler
 server = SocketServer.TCPServer(('0.0.0.0', 80), Handler)
 
-server.serve_forever()
+#server.serve_forever()
 
 def ButtonPlayer1 (pin):
-	print "Button player 1 pressed"
-	logger.info("Button player 1 pressed")
-	#Ignore debounce effect
-	global time_stamp
-	time_now = time.time()
-	if (time_now - time_stamp) >= 1.0:
-		print "Ready to scan."
-	time_stamp = time_now
-	#Claim point
-	try:
-		url = 'http://' + webserver + '/points/claim/' + venue + '/1'
-		values = {'key' : 'value' }
-		data = urllib.urlencode(values)
-		req = urllib2.Request(url, data)
-		response = urllib2.urlopen(req)
-		the_page = response.read()
-	except urllib2.HTTPError, e:
-		print e
-		logger.error(e)
+	print GPIO.input(23)
+	if GPIO.input(23) == 0:
+		#print "Button player 1 pressed"
+		logger.info("Button player 1 pressed")
+		#Claim point
+		try:
+			url = 'http://' + webserver + '/points/claim/' + venue + '/1'
+			values = {'key' : 'value' }
+			data = urllib.urlencode(values)
+			req = urllib2.Request(url, data)
+			response = urllib2.urlopen(req)
+			the_page = response.read()
+		except urllib2.HTTPError, e:
+			print e
+			logger.error(e)
 
-def SensorSide1 (pin):
-	print "Sensor side 1 detected movement"
-	#Add goal
-	try:
-		url = 'http://' + webserver + '/goals/add/' + venue + '/1'
+
+def SensorFired(hot, sensorNumber):
+	global goal
+
+	if hot:
+		goal = 0
+	else:
+		goal = goal+1
+
+	if goal == 1:
+		print "Made web request for goal @ sensor"
+		url = 'http://' + webserver + '/points/add/' + venue + '/' + str(sensorNumber)
+		print url
 		values = {'key' : 'value' }
 		data = urllib.urlencode(values)
 		req = urllib2.Request(url, data)
 		response = urllib2.urlopen(req)
 		the_page = response.read()
-	except urllib2.HTTPError, e:
-		print e
-		logger.error(e)
+		time.sleep(1)
+
+
+def ButtonFired(hot, buttonNumber):
+	global buttonDown
+
+	if hot:		
+		#if hot > 1
+		#	print "button losgelaten"
+		buttonDown = 0
+	else:
+		buttonDown = buttonDown+1
+
+	if buttonDown == 1:
+		print "Goal claimed using button " + str(buttonNumber)
+		time.sleep(0.5)
+
 
 def main():
 
     GPIO.setmode(GPIO.BCM)
 
     GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    time_stamp = time.time()
-    GPIO.remove_event_detect(23)
-    GPIO.remove_event_detect(24)
-    GPIO.add_event_detect(23,GPIO.RISING, callback=ButtonPlayer1, bouncetime=200)
-    GPIO.add_event_detect(24,GPIO.RISING, callback=ButtonPlayer1, bouncetime=200)
-    # time.sleep(5)
-
+    GPIO.setup(24, GPIO.IN)
+    #GPIO.add_event_detect(23,GPIO.FALLING, callback=ButtonPlayer1)
+    #GPIO.add_event_detect(24,GPIO.RISING, callback=SensorSide1)
+    
     while True:
-	os.system('clear')
-        print "Waiting for event."
-	time.sleep(1)
-	os.system('clear')
-        print "Waiting for event.."
-	time.sleep(1)
-	os.system('clear')
-        print "Waiting for event..."
-	time.sleep(1)
+	SensorFired(GPIO.input(24), 1)
+	ButtonFired(GPIO.input(23), 1)
+	
+	# for other sensors, add if statement below with sensornumber as second parameter
+	# SensorFired(true, 2)
 
 
 
+    print "cleanup"	
     GPIO.cleanup()
 
 
